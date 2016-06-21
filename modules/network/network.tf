a6c8452f979028b0fe30664b57188ba8bfc122e0
Jon Benson <bensojon@gmail.com>
2016-06-20 19:07:26 -0700

    master
    master

---
+++ modules/network/network.tf
@@ -0,0 +1,31 @@
+variable "name" { default = "network" }
+variable "cidr" { }
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.cidr}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_security_group" "allow" {
+  name   = "allow-${var.name}"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+output "vpc_id"   { value = "${aws_vpc.vpc.id}" }
+output "vpc_cidr" { value = "${aws_vpc.vpc.cidr_block}" }
