74f0abbe717c2c7a6efdc867ce45c4f380c7ea98
Pryz <ju.pryz@gmail.com>
2017-02-09 14:22:44 -0800

    foo

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 variable "name" { default = "REPLACE_ME" }
-variable "cidr" { default = "10.139.0.0/16" }
+variable "foo" { default = "REPLACE_ME" }
 
-module "network" {
-  source = "modules/network"
+module "beer" {
+  source = "modules/beer"
 
   name = "${var.name}"
-  cidr = "${var.cidr}"
+  foo = "${var.foo}"
 }
 
 output "env"      { value = "${var.name}" }
-output "vpc_cidr" { value = "${module.network.vpc_cidr}" }
+output "vpc_cidr" { value = "${module.beer.foo}" }
